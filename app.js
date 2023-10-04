1e942f05f06df7666631912e0db10a323bf9ef72
Zainabsoomro6789 <Zainabsoomro6789@gmail.com>
2023-10-05 03:23:28 +0500

    ready to commit

---
+++ app.js
@@ -0,0 +1,70 @@
+// Q 1
+// var userImput = prompt("enter city name ");
+// if(userImput === "karachi"){
+//     alert("welcome to city of lights")
+// }
+// q2
+// var userInput = prompt("enter");
+// if(userImput === "male"){
+//     alertt("good morning sir")
+// }else if(userImput === "female"){
+//     alert("good morning mam")
+// }
+// q3
+// var userInputr = prompt("enter");
+// if(userInputr === "red"){
+//     alert("must stop");
+// }else if(userInputr === "yellow"){
+//     alert("ready to move");
+// }else if(userInputr === "green"){
+//     alert("move now");
+// }
+// q4/
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// b
+// var  b = 82;
+// if (++b === 83){
+// alert("given condition for variable b is true");
+// }
+// c 
+// var c = 12;
+// if (++c === 13){
+// alert("condition");}
+// if (c === 13){
+//     alert("condition 2 is true");
+//     }
+//     if (++c < 14){
+//     alert("condition 3 is true");
+//     }
+//     if(c === 14){
+//     alert("condition 4 is true");
+//     }
+
+//  var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+//     alert("The cost equals");}
+
+
+// var firstSubject = +prompt("Enter first subject marks");
+// var secondSubject = +prompt("Enter second subject marks");
+// var thirdSubject = +prompt("Enter third subject marks");
+// var totalMarks = +prompt("Enter  totalMarks ");
+// var obtMarks = firstSubject + secondSubject + thirdSubject;
+// var percentage = obtMarks / totalMarks * 100;
+// if(percentage >=80 && percentage <=100){
+//     document.write("percentage" + percentage + "<br>" + "Grade:" + "A+" + "<br>" + "remark:" + "Excellent" );
+// }
+// else if (percentage >=70 && percentage <=80){
+//     document.write("percentage" + percentage + "<br>" + "Grade:" + "A" + "<br>" + "remark:" + "Good" );
+// }
+// else if (percentage >=60 && percentage <=70){
+//     document.write("percentage" + percentage + "<br>" + "Grade:" + "B" + "<br>" + "remark:" + "You need to improve" );
+// }
+// else if (percentage >=60 ){
+//     document.write("percentage" + percentage + "<br>" + "Grade:" + "Fail" + "<br>" + "remark:" + "Sorry" );
+// }
